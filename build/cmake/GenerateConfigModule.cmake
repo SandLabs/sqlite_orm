7df01b8a3db307ff9c29e5284358282bf4eb6a2c
m.shlyamov <m.shlyamov@yandex.com>
2020-02-22 00:38:56 +0400

    FIX install paths

--- build/cmake/GenerateConfigModule.cmake
+++ build/cmake/GenerateConfigModule.cmake
@@ -6,19 +6,19 @@ set(PACKAGE_CMAKE_INSTALL_DIR "${cmakedir}/sqlite_orm")
 # In CYGWIN enviroment below commands does not work properly
 if (NOT CYGWIN)
 		configure_package_config_file("${CMAKE_CURRENT_SOURCE_DIR}/build/cmake/SqliteOrmConfig.cmake.in"
-						"${CMAKE_CURRENT_BINARY_DIR}/SqliteOrmConfig.cmake"
+						"${CMAKE_CURRENT_BINARY_DIR}/sqlite_ormConfig.cmake"
 						INSTALL_DESTINATION "${CMAKE_INSTALL_DIR}/sqlite_orm"
 						PATH_VARS
 						PACKAGE_INCLUDE_INSTALL_DIR
 						PACKAGE_CMAKE_INSTALL_DIR
 						)
 
-		write_basic_package_version_file("${CMAKE_CURRENT_BINARY_DIR}/SqliteOrmConfigVersion.cmake"
+		write_basic_package_version_file("${CMAKE_CURRENT_BINARY_DIR}/sqlite_ormConfigVersion.cmake"
 						VERSION ${sqlite_orm_VERSION}
 						COMPATIBILITY SameMajorVersion
 						)
 
-		install(FILES "${CMAKE_CURRENT_BINARY_DIR}/SqliteOrmConfig.cmake"
-						"${CMAKE_CURRENT_BINARY_DIR}/SqliteOrmConfigVersion.cmake"
+		install(FILES "${CMAKE_CURRENT_BINARY_DIR}/sqlite_ormConfig.cmake"
+						"${CMAKE_CURRENT_BINARY_DIR}/sqlite_ormConfigVersion.cmake"
 						DESTINATION "${CMAKE_INSTALL_DIR}/sqlite_orm")
 endif()
